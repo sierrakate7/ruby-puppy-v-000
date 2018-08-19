b93014b6b4affb36d6bf15e60c210e078440a4df
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-19 03:26:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,20 @@ attr_accessor :name
 
 @@all = []
   
-  
-  
+def initialize(name)
+    @name = name
+    @@all << self
+ end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
   
   
   
